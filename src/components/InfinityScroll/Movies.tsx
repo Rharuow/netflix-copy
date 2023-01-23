d63c95ea82284c4e2d8350db6a37220adcb322e6
Harysson <haryssonsoares@gmail.com>
2023-01-23 12:17:36 -0300

    chore: clean code[

--- src/components/InfinityScroll/Movies.tsx
+++ src/components/InfinityScroll/Movies.tsx
@@ -3,54 +3,20 @@ import Image from "next/image";
 import TextTruncate from "react-text-truncate";
 
 import { IMovies } from "../../types/movies";
-import { IShowInfoMovies } from "../Resources/Movies/movies";
-import { useWindowSize } from "../../Hooks/windowsize";
 import { useRouter } from "next/router";
 
 const Movies: React.FC<{ movies: IMovies | Array<null> }> = ({ movies }) => {
-  const [showInfo, setShowInfo] = useState<IShowInfoMovies>([]);
-
-  const { isMobile, isTablet } = useWindowSize();
-
   const router = useRouter();
-
-  useEffect(() => {
-    movies.length &&
-      setShowInfo(
-        movies.map((movie, index) => ({
-          id: movie ? movie.id : index,
-          status: false,
-        }))
-      );
-  }, [movies]);
 
   return (
     <div className="d-flex flex-wrap justify-content-around">
       {movies.map((movie, index) => (
         <div
           key={index}
-          onMouseOver={() =>
-            setShowInfo((prevState) =>
-              prevState.map((info, index) => {
-                if (info?.id === movie?.id)
-                  return { id: info ? info.id : index, status: true };
-                return info;
-              })
-            )
-          }
           role="button"
           onClick={(index) => {
             movie && router.push(`/movies/${movie.id}`);
           }}
-          onMouseLeave={() =>
-            setShowInfo((prevState) =>
-              prevState.map((info) => {
-                if (info?.id === movie?.id)
-                  return { id: info ? info.id : index, status: false };
-                return info;
-              })
-            )
-          }
           className="d-flex mb-3 flex-column align-items-center animation-card-scroll"
         >
           <Image
